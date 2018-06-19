470f8c1871a8f478b3631dfd1506a296bc29518c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 15:22:22 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export default class CoordinatesButton extends React.Component {
+  onClick = function(e){
+    const xyCoordinates = [];
+    return this.props.onReceiveCoordinates()
+  }
   render() {
     return (
-      <button></button>
+      <button onClick={this.onClick}></button>
     );
   }
 }
